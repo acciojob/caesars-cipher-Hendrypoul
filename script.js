0fe3d2e18bbd5683373200e055f72db5bf941634
Hendrypoul <106142503+Hendrypoul@users.noreply.github.com>
2023-03-14 23:33:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,10 @@
-// Your Script here.
-function isCap(num) {
-	if(num >= 65 && num <= 90) {
-		return true;
-	}
-	return false;
-}
-function range(num, min, max) {
-	if(num >= min && num <= max) {
-		return num;
-	}
-	else {
-		return num - max + min - 1;
-	}
-}
-range(100, 65, 90);
 function rot13(str) {
-	str = str.toUpperCase();
-	var n = 0;
-	var l ="";
-	var str2 = "";
-	for(x = 0; x < str.length; x++) {
-		n = str.charCodeAt(x);
-		if (isCapLetter(n)) {
-			n = range(n + 13, 65, 90);
-		}
-		l = String.fromCharCode(n);
-		str2 += 1;
-	}
-	return str2;
+	const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	return str 
+		.split('')
+		.map(char => {
+			const pos = alphabet.indexOf(char);
+			return pos >= 0 ? alphabet[(pos + 13) % 26] : char;
+		})
+		.join('');
 }
-rot13("serR PBQR PNZC")
