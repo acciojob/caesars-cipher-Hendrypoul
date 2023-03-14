9558ae016b89fd773c85469b9a95cf87b710fbc1
Hendrypoul <106142503+Hendrypoul@users.noreply.github.com>
2023-03-14 23:54:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 function rot13(str) {
-	const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-	return str 
-		.split('')
-		.map(char => {
-			const pos = alphabet.indexOf(char);
-			return pos >= 0 ? alphabet[(pos + 13) % 26] : char;
-		})
-		.join('');
+	const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+	var decodedArr = [];
+	for(var i = 0; i < str.length; i++) {
+		var newArr = abc.indexOf(str[i]) + 13;
+	if(abc.indexOf(str[i]) == -1) {
+		decodeArr.push(str[i]);
+	}
+	else {
+		decodeArr.push(abc[newArr]);
+	}
+	}
+	return decodeArr.join("");
 }
+rot13("SERR PBQR PNZC")
